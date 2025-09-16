18d442e5ff7ea6e6ff84bad70ac79485ffff1e2a
Kmsinchana <86287168+Kmsinchana@users.noreply.github.com>
2025-09-16 09:11:09 +0530

    added the semicolon changes

--- 01_test.js
+++ 01_test.js
@@ -1,1 +1,11 @@
 console.log("Hi Everyone")
+// adding the semicolon before ([ brackets
+// here it will try to assign function to a if we doesn't add the semicolon
+//error we ge without semicolon
+/*TypeError: 8 is not a function
+    at Object.<anonymous> (C:\Users\DELL\Desktop\Java_Script_practice\JavaScript_practice\01_test.js:5:1)
+*/
+let a = 8;
+(function(){
+    console.log("hello function")
+})()
